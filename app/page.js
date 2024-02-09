6ca85cf754757fb833417251e821077ac0fb684a
Gankhulug02 <huluguu0202@gmail.com>
2024-02-09 21:06:24 +0800

    fix: send email with name

--- app/page.js
+++ app/page.js
@@ -20,9 +20,11 @@ const sentences = [
 ];
 
 export default function Home() {
+  const [name, setName] = useState('');
+  const [nerBut, setNerBut] = useState(false);
   const [yesFontSize, setYesFontSize] = useState(20);
-  const [iseYesClicked, setIsYesClicked] = useState(false);
   const [noClickCount, setNoClickCount] = useState(0);
+  const [iseYesClicked, setIsYesClicked] = useState(false);
   const [gifUrl, setGifUrl] = useState(
     'https://media0.giphy.com/media/KZAMZlHBmqZ6Zu0HBO/200w.webp'
   );
@@ -34,7 +36,7 @@ export default function Home() {
         'template_qq20nkt',
         {
           user_name: 'user_name',
-          to_name: 'to_name',
+          to_name: name,
           from_name: 'from_name',
           user_email: 'azure.gankhulug@gmail.com',
           message: 'darchihje',
@@ -68,40 +70,62 @@ export default function Home() {
 
   return (
     <div className="h-screen w-full flex flex-col gap-[20px] justify-center  items-center">
-      <div className="w-[350px] aspect-square ">
-        <Image
-          src={gifUrl}
-          alt="img"
-          width={5000}
-          height={5000}
-          className="w-full h-full object-contain rounded-lg"
-        />
-      </div>
-      {iseYesClicked === false ? (
-        <p className="text-2xl">Will you be my Valentine?</p>
-      ) : (
-        <p className="text-2xl">You are mine now</p>
-      )}
-
-      {iseYesClicked === false ? (
+      {name !== '' && nerBut ? (
         <>
-          <div className="flex items-center gap-6">
-            <button
-              className={`bg-green-400 rounded-md px-4 py-2 `}
-              style={{ fontSize: `${yesFontSize}px` }}
-              onClick={yesFun}
-            >
-              Yes
-            </button>
-            <button
-              className="bg-red-500 h-fit w-fit rounded-md px-4 py-2 text-[20px]"
-              onClick={noFun}
-            >
-              {sentences[noClickCount]}
-            </button>
+          <div className="w-[350px] aspect-square ">
+            <Image
+              src={gifUrl}
+              alt="img"
+              width={5000}
+              height={5000}
+              className="w-full h-full object-contain rounded-lg"
+            />
           </div>
+          {iseYesClicked === false ? (
+            <p className="text-2xl">Will you be my Valentine?</p>
+          ) : (
+            <p className="text-2xl">You are mine now</p>
+          )}
+
+          {iseYesClicked === false ? (
+            <>
+              <div className="flex items-center gap-6">
+                <button
+                  className={`bg-green-400 rounded-md px-4 py-2 `}
+                  style={{ fontSize: `${yesFontSize}px` }}
+                  onClick={yesFun}
+                >
+                  Yes
+                </button>
+                <button
+                  className="bg-red-500 h-fit w-fit rounded-md px-4 py-2 text-[20px]"
+                  onClick={noFun}
+                >
+                  {sentences[noClickCount]}
+                </button>
+              </div>
+            </>
+          ) : null}
         </>
-      ) : null}
+      ) : (
+        <div className="flex gap-2 text-[#565656]">
+          <input
+            className="p-2 rounded-lg w-[250px]"
+            placeholder="Нэрээ бичээрэй хөөрхөнөө"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          <button
+            className="bg-white p-2 rounded-lg"
+            onClick={() => {
+              setNerBut(true);
+            }}
+          >
+            Next
+          </button>
+        </div>
+      )}
     </div>
   );
 }
