94ad69bffc37c6a394bda4033426caee47e26fd2
gankhulug1 <gankhulug@bananamall.mn>
2024-02-28 10:50:23 +0800

    hotjar done?

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from 'next/head';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +13,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+       <head>
+        {/* Other head elements */}
+        {typeof document !== 'undefined' && (
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:3884300,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+              `,
+            }}
+          />
+        )}
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
